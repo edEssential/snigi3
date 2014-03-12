1b4c62af8e3a9dff4f8c6ceac24e08d551a6fe55
edEssential <ed@essential-music.com>
2014-03-12 12:17:08 +0000

    jquery alt version working

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'csv_builder'
 gem 'sass-rails',   '~> 3.2.3'
 gem 'coffee-rails', '~> 3.2.1'
 gem 'uglifier', '>= 1.0.3'
+gem 'jquery-rails', '2.1.3'
 
 group :development, :test do
   gem 'sqlite3'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require userSpecJquery
+//= require jquery
+//= require jquery_ujs
 
-$(document).ready(function() {
+jQuery(document).ready(function() {
+	
 });
