09ea28af68beddf508210cbf14d20d81c9e44c07
edEssential <ed@essential-music.com>
2014-03-12 10:31:49 +0000

    moved assets to rails conventions

---
+++ app/assets/javascripts/visia.js
@@ -0,0 +1,16 @@
+//= require retina
+//= require smoothscroll
+//= require waypoints
+//= require parallax
+//= require navigation
+//= require jquery.mixitup
+//= require jquery.easing
+//= require jquery.fittext
+//= require jquery.localscroll
+//= require jquery.scrollto
+//= require jquery.appear
+//= require jquery.waitforimages
+//= require jquery.bxslider
+//= require jquery.fitvids
+//= require main
+//= require shortcodes
