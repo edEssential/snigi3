ce68fe21350ad347fe25796d180634460314903a
edEssential <ed@essential-music.com>
2014-03-12 17:06:29 +0000

    shows ordered by date descending

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def bring_in_models
-    @upcoming_shows = Show.upcoming_shows
-    @past_shows = Show.past_shows
+    @upcoming_shows = Show.upcoming_shows.order("date DESC")
+    @past_shows = Show.past_shows.order("date DESC")
     @releases = Release.order("id DESC")
     @videos = Video.order("id DESC")
     @contact = Contact.new
