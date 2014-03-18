abff0e1d406408be03cdf250b30a422f368af647
edEssential <ed@essential-music.com>
2014-03-18 22:24:06 +0000

    contact form button styling for mobile

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,11 +42,10 @@ jQuery(document).ready(function() {
 			url: "./releases/release_show_via_ajax_call",
 			data: {id: id},
 			success: function(data) {
-				var padder = document.getElementById('releaseHeader');
 				var release_content = SMT['releaseshow'](data);
 				jQuery('#releaseShowWrapper').show(function(){
 					jQuery('#releaseShowContent').empty().hide().append(release_content).fadeIn(700);
-					padder.scrollIntoView(true);
+					jQuery(this).prev('.container').scrollTop();
 				});
 			}
 		});
