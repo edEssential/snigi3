7d106df02eba3309cd8fb651ce0cece07fb4eeab
madebyeggs <madebyeggs@gmail.com>
2015-08-13 17:07:46 +0100

    shows past date changed

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   attr_accessible :artist, :date, :location, :venue, :buylink
   
   def self.past_shows
-    where('date <?', Date.today).where('date >?', Date.today.ago(1.year))
+    where('date <?', Date.today).where('date >?', Date.today.ago(3.years))
   end
   
   def self.upcoming_shows
